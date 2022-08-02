31a4d9b301087c8e6f2660d6146f0ce8fd157f8a
Inokcode <regilearn@gmail.com>
2022-08-02 16:32:57 +0530

    useStates added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Register from "./Register";
 
 function App() {
   return (
     <main className="App">
+      123
       <Register />
     </main>
   );
